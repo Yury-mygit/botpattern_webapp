f569e7f63ea283124ce8738b41be70bb04b1cfdc
YURIY <yury.myworkmail@gmail.com>
2023-11-11 11:37:27 +0300

    i'm working on calendar. session are in work

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -42,10 +42,13 @@ const Footer: React.FC<FooterProps> = ({})=>{
 return(<div>Footer</div>)
 }
 
+
+
+
+
+
 export function Calendar() {
   const [currentMonth, setCurrentMonth] = React.useState(new Date());
-
-
 
 
   const prevMonth = () => {
@@ -78,15 +81,10 @@ export function Calendar() {
   const currentDate = new Date();
   currentDate.setHours(0, 0, 0, 0); // Normalize to compare only year, month, and day
 
+  const [selectedDay, setSelectedDay] = React.useState<Date | null>(null);
+  const [meetings, setMeetings] = React.useState<{[key: string]: {specialist: string, time: string}}>({});
 
-prevMonthDays.concat(days).concat(nextMonthDays).map((day, index) => {
-  const dayDate = new Date(currentMonth.getFullYear(), currentMonth.getMonth(), day);
-  dayDate.setHours(0, 0, 0, 0);
-  console.log(currentMonth);
-  return '';
-});
-
-const generateCalendarDays = (daysArray: number[], monthOffset: number) => {
+  const generateCalendarDays = (daysArray: number[], monthOffset: number) => {
     return daysArray.map((day, index) => {
       const dayDate = new Date(currentMonth.getFullYear(), currentMonth.getMonth() + monthOffset, day);
       dayDate.setHours(0, 0, 0, 0); // Normalize to compare only year, month, and day
@@ -94,14 +92,58 @@ const generateCalendarDays = (daysArray: number[], monthOffset: number) => {
         <div
             key={index}
             className={`w-12 h-12 border border-gray-500 flex items-center justify-center 
-            ${dayDate < currentDate ? 'text-gray-400' : ''}`}>
+            ${dayDate < currentDate ? 'text-gray-400' : ''} ${dayDate.getTime() === selectedDay?.getTime() ? 'bg-blue-200' : ''}`}
+            onClick={() => setSelectedDay(dayDate)}>
           {day}
         </div>
       );
     });
   };
 
- return (
+    const handleRegister = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const specialist = formData.get('specialist') as string;
+    const time = formData.get('time') as string;
+    setMeetings(prevMeetings => ({...prevMeetings, [selectedDay!.toISOString()]: {specialist, time}}));
+  };
+
+    type SessionProps = {
+  selectedDay: Date | null;
+  meetings: {[key: string]: {specialist: string, time: string}};
+  onRegister: (event: React.FormEvent<HTMLFormElement>) => void;
+};
+
+   const Session: React.FC<SessionProps> = ({selectedDay, meetings, onRegister}) => {
+  if (!selectedDay) return null;
+
+  return (
+    <>
+      <p>Dear customer, you are registering for a session on {selectedDay.toLocaleDateString()}</p>
+      <form onSubmit={onRegister}>
+        <label>
+          Specialist:
+          <input type="text" name="specialist" required />
+        </label>
+        <label>
+          Time:
+          <input type="time" name="time" required />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+      {meetings[selectedDay.toISOString()] && (
+        <div>
+          <h2>Scheduled meeting:</h2>
+          <p>Specialist: {meetings[selectedDay.toISOString()].specialist}</p>
+          <p>Time: {meetings[selectedDay.toISOString()].time}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+
+return (
     <div className="callendar_wrapper w-96">
       <Header prevMonth={prevMonth} nextMonth={nextMonth} currentMonth={currentMonth}/>
       <div className="grid grid-cols-7 gap-1">
@@ -109,9 +151,8 @@ const generateCalendarDays = (daysArray: number[], monthOffset: number) => {
         {generateCalendarDays(days, 0)}
         {generateCalendarDays(nextMonthDays, 1)}
       </div>
+      <Session selectedDay={selectedDay} meetings={meetings} onRegister={handleRegister} />
       <Footer />
     </div>
   );
-
-
 }
